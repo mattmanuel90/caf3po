664d77bb37f6dd602bbcc5249e2196bbd6034798
Matthew Manuel <manuel.matthew90@gmail.com>
2017-09-08 11:47:31 +0100

    Fix more indentation issues.

--- src/zendesk.js
+++ src/zendesk.js
@@ -2,42 +2,42 @@ var Zendesk = require("zendesk-node-api");
 var moment = require("moment");
 
 var zendesk = new Zendesk({
-	url: process.env.ZENDESK_URL,
-	email: process.env.ZENDESK_EMAIL,
-	token: process.env.ZENDESK_TOKEN
+  url: process.env.ZENDESK_URL,
+  email: process.env.ZENDESK_EMAIL,
+  token: process.env.ZENDESK_TOKEN
 });
 
 const groupTickets = (tickets) => {
-	return new Promise((resolve, reject) => {
-		let groupedTickets = [];
-		for (let ticket of tickets) {
-			if(!groupedTickets[ticket.status]) {
-				groupedTickets[ticket.status] = [];
-			}
-			groupedTickets[ticket.status].push({
-				id:ticket.id,
-				status: ticket.status,
-				subject: ticket.subject,
-				updated_at: ticket.updated_at
-			});
-		}
-		resolve(groupedTickets);
-	});
+  return new Promise((resolve, reject) => {
+    let groupedTickets = [];
+    for (let ticket of tickets) {
+      if(!groupedTickets[ticket.status]) {
+        groupedTickets[ticket.status] = [];
+      }
+      groupedTickets[ticket.status].push({
+        id:ticket.id,
+        status: ticket.status,
+        subject: ticket.subject,
+        updated_at: ticket.updated_at
+      });
+    }
+    resolve(groupedTickets);
+  });
 }
 
 exports.getZendeskTickets = async (status) => {
-	let text = [];
-	let groupedTickets = await zendesk.search
-	.list(`query=type:ticket status<closed`)
-	.then(tickets => groupTickets(tickets));
+  let text = [];
+  let groupedTickets = await zendesk.search
+  .list(`query=type:ticket status<closed`)
+  .then(tickets => groupTickets(tickets));
 
-	for (let group in groupedTickets ) {
-		let message = `You have ${groupedTickets[group].length} ${group} zendesk tickets`;
-		let listofTickets = '';
-		for (let ticket of groupedTickets[group]) {
-			listofTickets += `><${process.env.ZENDESK_URL}/agent/tickets/${ticket.id}|#${ticket.id}> - ${ticket.subject} *Last Updated: ${moment(ticket.updated_at).fromNow()}*\n`;
-		}
-		text.push({message,listofTickets});
-	}		
-	return text;
+  for (let group in groupedTickets ) {
+    let message = `You have ${groupedTickets[group].length} ${group} zendesk tickets`;
+    let listofTickets = '';
+    for (let ticket of groupedTickets[group]) {
+      listofTickets += `><${process.env.ZENDESK_URL}/agent/tickets/${ticket.id}|#${ticket.id}> - ${ticket.subject} *Last Updated: ${moment(ticket.updated_at).fromNow()}*\n`;
+    }
+    text.push({message,listofTickets});
+  }   
+  return text;
 };
